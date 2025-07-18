be00b5ef7ee6473931c439c3b905f4a60e4ef322
1194314874@qq.com <zeng123456>
2025-07-18 14:09:17 +0800

    添加文件

--- src/main/java/com/zengyanyu/system/SpringBootSystemApplication.java
+++ src/main/java/com/zengyanyu/system/SpringBootSystemApplication.java
@@ -1,5 +1,10 @@
 package com.zengyanyu.system;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableSwagger2
 @SpringBootApplication
 @MapperScan(value = {"com.zengyanyu.**.mapper"})
