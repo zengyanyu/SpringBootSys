42bfea4f6d4fa19da118e7d0fbf53d2fc405f754
1194314874@qq.com <1194314874@qq.com>
2026-02-26 22:06:50 +0800

    添加

--- src/main/java/com/zengyanyu/system/entity/DictCopy1.java
+++ src/main/java/com/zengyanyu/system/entity/DictCopy1.java
@@ -3,19 +3,14 @@ package com.zengyanyu.system.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
- * 
- *
  * @author zengyanyu
  * @since 2026-02-26
  */
@@ -24,7 +19,7 @@ import lombok.Setter;
 @Entity
 @TableName("dict_copy1")
 @ApiModel(value = "DictCopy1对象", description = "")
-public class DictCopy1 implements Serializable {
+public class DictCopy1 extends BaseEntity {
 
     @Id
     @TableId(value = "id", type = IdType.AUTO)
@@ -33,6 +28,5 @@ public class DictCopy1 implements Serializable {
     private String code;
 
     private String name;
-
 
 }
