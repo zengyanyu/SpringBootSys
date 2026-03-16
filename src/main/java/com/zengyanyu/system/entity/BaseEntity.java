8cfc2572e000f2d4d4609b04273e173cf1d289e5
zengyanyu <1194314874@qq.com>
2026-03-16 09:35:39 +0800

    优化

--- src/main/java/com/zengyanyu/system/entity/BaseEntity.java
+++ src/main/java/com/zengyanyu/system/entity/BaseEntity.java
@@ -11,14 +11,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-import net.bytebuddy.implementation.bind.annotation.Super;
 
-import javax.annotation.processing.SupportedOptions;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.LocalDateTime;
