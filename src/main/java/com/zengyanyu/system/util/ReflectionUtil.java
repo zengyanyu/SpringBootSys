cb6beda030a43be8d8532671600cf234d0c1fec9
zengyanyu <1194314874@qq.com>
2026-03-26 14:32:07 +0800

    优化

--- src/main/java/com/zengyanyu/system/util/ReflectionUtil.java
+++ src/main/java/com/zengyanyu/system/util/ReflectionUtil.java
@@ -5,10 +5,12 @@
  */
 package com.zengyanyu.system.util;
 
+import com.zengyanyu.system.entity.Department;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 
 /**
  * 反射工具类
@@ -26,14 +28,39 @@ public class ReflectionUtil {
      * @return
      */
     public static Object getValue(Object object, String fieldName) {
-        try {
-            // 若使用超类里面的字段，请使用getSuperclass()方法
-            Field field = object.getClass().getSuperclass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (Exception e) {
-            e.getMessage();
+        // 空值校验
+        if (object == null) {
+            throw new IllegalArgumentException("对象不能为空！");
         }
-        return null;
+
+        Class<?> clazz = object.getClass();
+
+        // 循环向上遍历所有父类查找字段
+        while (clazz != null) {
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field.get(object);
+            } catch (NoSuchFieldException e) {
+                // 当前类找不到，继续找父类
+                clazz = clazz.getSuperclass();
+            } catch (Exception e) {
+                throw new RuntimeException("获取字段值失败：" + fieldName, e);
+            }
+        }
+
+        // 遍历完所有类都没找到 → 直接抛明确异常
+        throw new NoSuchFieldError("在类【" + object.getClass().getName() + "】及其父类中，未找到字段：" + fieldName);
     }
+
+    public static void main(String[] args) {
+        Department dept = new Department();
+        dept.setDeptName("1111111111");
+        dept.setCreateTime(LocalDateTime.now());
+        Object deptName = getValue(dept, "deptName");
+        System.out.println("deptName = " + deptName);
+        Object createTime = getValue(dept, "createTime");
+        System.out.println("createTime = " + createTime);
+    }
+
 }
