77ecccb7ec873c78bea23d2e4ac0b53c3c58fe42
zengyanyu <1194314874@qq.com>
2026-04-29 11:36:07 +0800

    优化动态日期转换器

--- src/main/java/com/zengyanyu/system/converter/DateConverter.java
+++ src/main/java/com/zengyanyu/system/converter/DateConverter.java
@@ -9,34 +9,43 @@ import com.alibaba.excel.converters.Converter;
 import com.alibaba.excel.enums.CellDataTypeEnum;
 import com.alibaba.excel.metadata.GlobalConfiguration;
 import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.metadata.property.DateTimeFormatProperty;
 import com.alibaba.excel.metadata.property.ExcelContentProperty;
+import com.alibaba.excel.util.StringUtils;
 
 import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
- * 日期转换器
+ * 动态日期转换器（支持自定义格式）<br>
+ * 这个转换器可以配合 com.alibaba.excel.annotation.format.DateTimeFormat.@DateTimeFormat("yyyy") 阿里巴巴的包配合使用，可以设置自定义的格式,默认为yyyy-MM-dd<br>
+ * 注意点：不使用 org.springframework.format.annotation.DateTimeFormat.@DateTimeFormat(pattern = "yyyy") 这个spring框架中的注解，对这个转换器是无效的
  *
  * @author zengyanyu
  */
 public class DateConverter implements Converter<Date> {
 
-    private static final String PATTERN_YYYY_MM_DD = "yyyy-MM-dd";
+    /**
+     * 默认格式
+     */
+    private static final String DEFAULT_PATTERN = "yyyy-MM-dd";
 
     /**
      * 代表：我这个转换器专门处理 Java 里的 Date 类型字段
      *
-     * @return
+     * @return Date类
      */
     @Override
-    public Class<Date> supportJavaTypeKey() {
+    public Class<?> supportJavaTypeKey() {
         return Date.class;
     }
 
     /**
      * 代表：Excel 里存的是字符串（比如 "2025-01-01"）
      *
-     * @return
+     * @return 字符串
      */
     @Override
     public CellDataTypeEnum supportExcelTypeKey() {
@@ -46,17 +55,26 @@ public class DateConverter implements Converter<Date> {
     /**
      * 写入 Excel 时调用：Date → String
      *
-     * @param value
-     * @param excelContentProperty
-     * @param globalConfiguration
-     * @return
-     * @throws Exception
+     * @param value               数据值
+     * @param contentProperty     内容属性
+     * @param globalConfiguration 全局配置
+     * @return Excel单元格数据
      */
     @Override
-    public WriteCellData<?> convertToExcelData(Date value, ExcelContentProperty excelContentProperty, GlobalConfiguration globalConfiguration) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat(PATTERN_YYYY_MM_DD);
-        String dateValue = sdf.format(value);
-        return new WriteCellData(dateValue);
+    public WriteCellData<?> convertToExcelData(Date value, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
+        // 1. 获取注解上的格式
+        String pattern = DEFAULT_PATTERN;
+
+        if (contentProperty != null) {
+            DateTimeFormatProperty prop = contentProperty.getDateTimeFormatProperty();
+            if (prop != null && StringUtils.isNotBlank(prop.getFormat())) {
+                pattern = prop.getFormat();
+            }
+        }
+
+        // 线程安全的时间格式化
+        String dateValue = DateTimeFormatter.ofPattern(pattern).withZone(ZoneId.systemDefault()).format(value.toInstant());
+        return new WriteCellData<>(dateValue);
     }
 
 }
